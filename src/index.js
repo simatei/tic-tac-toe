cfb88642087fe4b23f494c6c5fe661fe1bcfc357
simatei <dr.robsimatei@gmail.com>
2019-08-26 11:32:58 +0300

    Define handleClick method on board component

--- src/index.js
+++ src/index.js
@@ -12,18 +12,34 @@ class Square extends React.Component {
     render() {
       return (
         <button className="square"
-         onClick={() => this.setState({value: 'X'})}
+         onClick={() => this.props.onClick()}
          >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i} />;
+    constructor(props){
+      super(props);
+      this.state ={
+        squares: Array(9).fill(null), //set board initial state to contain array of nine nulls
+      };
     }
+
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
+    renderSquare(i) {
+      return <Square
+       value={this.state.squares[i]}
+       onClick={() => this.handleClick(i)}
+        />;
+    };
   
     render() {
       const status = 'Next player: X';
