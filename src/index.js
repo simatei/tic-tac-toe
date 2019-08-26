05a915d6d4d2e89eaa112b79b74e99f96c7ac4a9
simatei <dr.robsimatei@gmail.com>
2019-08-26 12:11:16 +0300

    Change handleClick function to return early if game won or square filled

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ class Board extends React.Component {
   handleClick(i){
     const squares = this.state.squares.slice(); // slice creates a copy of the array
                                                 // to copy instead of modifying the existing array
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X': 'O';
     this.setState(
       {squares: squares,
