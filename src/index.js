afb7572bc3561fea7034d7161fad3e01c1d34c65
simatei <dr.robsimatei@gmail.com>
2019-08-26 12:33:00 +0300

    Modify Game component to render game status

--- src/index.js
+++ src/index.js
@@ -86,13 +86,26 @@ class Game extends React.Component {
     }
   }
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner){
+      status = 'Winner ' + winner;
+    } else{
+      status = 'Next Player ' + (this.state.xIsNext ? 'X':'O')
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+          squares = {current.squares}
+          onClick = {(i) => this.handleClick(i)}
+           />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
